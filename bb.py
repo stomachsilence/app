98b2913698c711c8ec9a4c893f461f49c72522d1
stomachsilence <89380678+stomachsilence@users.noreply.github.com>
2021-09-02 09:58:09 +0900

    Update bb.py

--- bb.py
+++ bb.py
@@ -51,31 +51,33 @@ twd = {"阪神":False,
 
 
 def live_scores():
+    try:
+        url = 'http://baseball.yahoo.co.jp/npb/schedule/'
+        res = requests.get(url)
+        res.raise_for_status()
 
-    url = 'http://baseball.yahoo.co.jp/npb/schedule/'
-    res = requests.get(url)
-    res.raise_for_status()
+        soup = BeautifulSoup(res.text, 'html.parser')
 
-    soup = BeautifulSoup(res.text, 'html.parser')
-    
-    games = soup.select('li.bb-score__item')
-    
-    for i in range(len(games)):
-        score = games[i].find(class_ = 'bb-score__link').get_text()
-        home = games[i].find(class_ = 'bb-score__homeLogo').get_text()
-        away = games[i].find(class_ = 'bb-score__awayLogo').get_text()
-        print(score,home,away)
-        if score[:1] == "7":
-            if score[-1:] == "表":
-                txt = "7回表" + team[away] + "のラッキーセブンでございます。 ファンの方、なおいっそうのご声援をお願いいたします。 #" + team[away] + " #" + away
-                if twd[away] == False:
-                    tweet(txt)
-                    twd[away] = True
-            elif score[-1:] == "裏":
-                txt = "7回裏" + team[home] + "のラッキーセブンでございます。 ファンの方、なおいっそうのご声援をお願いいたします。 #" + team[home] + " #" + home
-                if twd[home] == False:
-                    tweet(txt)
-                    twd[home] = True
+        games = soup.select('li.bb-score__item')
+
+        for i in range(len(games)):
+            score = games[i].find(class_ = 'bb-score__link').get_text()
+            home = games[i].find(class_ = 'bb-score__homeLogo').get_text()
+            away = games[i].find(class_ = 'bb-score__awayLogo').get_text()
+            print(score,home,away)
+            if score[:1] == "7":
+                if score[-1:] == "表":
+                    txt = "7回表" + team[away] + "のラッキーセブンでございます。 ファンの方、なおいっそうのご声援をお願いいたします。 #" + team[away] + " #" + away
+                    if twd[away] == False:
+                        tweet(txt)
+                        twd[away] = True
+                elif score[-1:] == "裏":
+                    txt = "7回裏" + team[home] + "のラッキーセブンでございます。 ファンの方、なおいっそうのご声援をお願いいたします。 #" + team[home] + " #" + home
+                    if twd[home] == False:
+                        tweet(txt)
+                        twd[home] = True
+    except:
+        pass
 
 
 while True:
